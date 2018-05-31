76d183f36c403cc8492d50d0d877972e048cfdb1
Timwi <github@timwi.de>
2018-05-31 19:12:39 +0200

    First commit

---
+++ Assets/SimonShrieksModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimonShrieks;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Simon Shrieks
+/// Created by Timwi
+/// </summary>
+public class SimonShrieksModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
